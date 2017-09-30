599480cd0fbb91fee7abc98b903851aeb59a0f67
BraxtonS <braxton.smith01@gmail.com>
2017-09-30 10:34:05 -0400

    Index

---
+++ index.php
@@ -0,0 +1,52 @@
+<hmtl>
+    <head>
+        <title>NCAT T.A.A.</title>
+        <link rel="icon" type="image/png" href="favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+    </head>
+
+
+    
+    <body>
+            <div class="w3-container w3-indigo">
+             <div class="w3-third w3-container"></div>
+                <div class=" w3-third w3-container">
+                <h1>NCAT T.A.A.</h1>
+                    <form>
+                        <input type="textfield" placeholder="Search">
+                    </form>
+                </div>
+              <div class="w3-third w3-container"></div>
+            </div>
+            
+            <div class="w3-third w3-container"></div>
+            <div class="w3-third w3-panel">
+                <div class="w3-container">
+                  <h2>Input Form</h2>
+                </div>
+                
+                <form class="w3-container">
+                  <p>
+                  <input class="w3-input" type="text">
+                  <label>Username</label></p>
+                
+                  <p>
+                  <input class="w3-input" type="text">
+                  <label>Last Name</label></p>
+                
+                  <p>
+                  <input class="w3-input" type="text">
+                  <label>Email</label></p>
+                  
+                  <button type="submit">Login</button>
+                </form>
+                </div>
+                
+                    
+            <div class="w3-third w3-container"></div>
+    </body>
+    
+    
